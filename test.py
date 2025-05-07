f9fc034d7a8742bb07689185704600b5a0580452
노현승 <seunglee0807@gmail.com>
2025-05-07 20:13:57 +0900

    수정

--- test.py
+++ test.py
@@ -8,5 +8,8 @@ with col2:
     st.title("here is column2 title")
     st.checkbox("this is checkbox1 in col2 ")
 
+col1.subheader("i am column1 subheader")
+col2.checkbox("this i checkbox2 in col2")
+
 
  

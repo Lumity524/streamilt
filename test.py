f48335ddd1d7377628e524161fe9b5229e163568
노현승 <seunglee0807@gmail.com>
2025-05-07 20:10:37 +0900

    변경

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-import streamlit as st
+import streamlit as st # type: ignore
 
-st.title('title')
-st.header('header')
-st.subheader('subheader')
+col1, col2 = st.columns([2, 3])
+with col1 :
+    st.title("here is column1 title")
+
+with col2:
+    st.title("here is column2 title")
+    st.checkbox("this is checkbox1 in col2 ")
 
 
  
